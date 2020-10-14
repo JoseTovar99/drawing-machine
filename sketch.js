e55feed64f6ac6f061b304a6591b955a9f36f210
JoseTovar510 <42582683+JoseTovar510@users.noreply.github.com>
2020-10-13 17:35:59 -0700

    FirstDrawing

--- sketch.js
+++ sketch.js
@@ -1,7 +1,44 @@
+let array = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(68, 182, 184);
+  strokeWeight(5)
+  noFill();
 }
 
 function draw() {
-  background(220);
+  if (mouseIsPressed == true) {
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    background(0);
+    array.push([mouseX, mouseY]);
+    // stroke(map(mouseX, 0, 600, 0, 255, true))
+    // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+
+
+  }
+
+
+}
+
+function keyTyped() {
+  if (key === 's') {
+
+    saveCanvas('filename', 'png');
+  } else if (key === 'd') {
+
+background(68, 182, 184);
+
+beginShape();
+    for(let i = 0; i < array.length; i++) {
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1])
+    }
+    endShape();
+
+
+  }
+
+  return false;
+
 }
