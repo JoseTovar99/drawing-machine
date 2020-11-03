02603dcbfe3b54399aa554a58b9a2eb352958f92
JoseTovar510 <42582683+JoseTovar510@users.noreply.github.com>
2020-11-02 23:16:12 -0800

    Final

--- DrawingMachine2/sketch.js
+++ DrawingMachine2/sketch.js
@@ -1,30 +1,33 @@
 let array = [];
-let strokeWidth = 8;
-
-
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 
 function setup() {
-  background(68, 182, 184);
-  createCanvas(600, 500);
-
+  createCanvas(windowWidth, windowHeight);
+  //background(68, 182, 184);
+  drawGrid();
+  noFill();
 
 }
 
 function draw() {
 
   background(68, 182, 184, 5);
+  strokeWeight(strokeWidth);
 
-  if (mouseIsPressed) {
-    strokeWeight(strokeWidth);
-      line(width - pmouseX, height - pmouseY, width - mouseX, height - mouseY);
-    }
-    //line(mouseX, mouseY, pmouseX, pmouseY);
+
+  noiseOffset += 0.03;
+  strokeWidth = noise(noiseOffset) * 50;
+
+    line(mouseX, mouseY, pmouseX, pmouseY);
 
      stroke(map(mouseX, 250, 600, 200, 100,))
     // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
 
-}
 
+
+
+}
 
 function keyTyped() {
   if (key === 's') {
@@ -35,5 +38,6 @@ function keyTyped() {
 
   }
 
+  return false;
 
 }
